30408b6a310fcee992326c0a3622ec36d6530f5c
Krivax <krivahz@gmail.com>
2019-01-01 20:33:35 -0300

    Desafio 015

--- js/dl-store.js
+++ js/dl-store.js
@@ -1,15 +1,52 @@
 
 const $heart = document.querySelector(".-heart");
 const $stars = document.querySelectorAll(".star");
+const positionLast = $stars.length -1;
 
 $heart.addEventListener("click", handleClick);
 
-$stars.forEach(function ($star){
-    $star.addEventListener("click", handleClick);
-})
+$stars.forEach(function($star, key){
+     if(key == 0){
+    $star.addEventListener('click', firstStar);
+    }
 
-function handleClick(){
- 
-   this.classList.toggle("-active");
+     if(key == positionLast){
+         $star.addEventListener("click", lastStar);
+    }
+
+     if(key > 0 && key < positionLast){
+         $star.addEventListener("click", function () {
+             middleStar(key); 
+         });
+     }
+});
+
+
+function middleStar(index){
+    $stars.forEach(function($star, key){
+        $star.classList.remove("-active");
+        if (key <= index){
+            $star.classList.add("-active");
+            }
+    });
+
+
 }
 
+function firstStar(){
+    $stars.forEach(function($star){
+        $star.classList.remove("-active");
+    });
+ 
+   this.classList.add("-active");
+}
+
+function lastStar() {
+    $stars.forEach(function ($star) {
+        $star.classList.add("-active");
+    });
+}
+
+function handleClick(){
+    this.classList.toggle("-active");
+}
